39c509dbdac01ea765891708c7f991d1b5000e6d
BriceLucifer <2376671337@qq.com>
2025-09-01 13:33:43 +0800

    zero start

---
+++ README.md
@@ -0,0 +1,4 @@
+# Zero To Production
+
+## Overview:
+1. a project which is for training for using rust in web dev
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpResponse, HttpResponseBuilder, HttpServer, Responder, web};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("hello {}\n", &name)
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            // .route("/", web::get().to(greet))
+            // .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        assert!(response.status().is_success())
+    }
 }
